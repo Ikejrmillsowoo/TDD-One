d8c484c8ab440ba0825ee791493893766edadf9f
Isaac <isaac@Zipcoders-MacBook-Pro-2.local>
2025-03-06 17:29:11 -0500

    StringUtils.java completed by passing all tests

--- TDDOne/tddone/src/main/java/rocks/zipcode/StringUtils.java
+++ TDDOne/tddone/src/main/java/rocks/zipcode/StringUtils.java
@@ -15,18 +15,28 @@ public class StringUtils {
     
     public static String reverse(String str) {
         // Your implementation here
-        return null;
+        StringBuilder sb = new StringBuilder(str);
+        return String.valueOf(sb.reverse());
     }
     
     public static int countOccurrences(String str, char c) {
         // Your implementation here
-        return 0;
+        int count = 0;
+        char[] arrToCount = str.toCharArray();
+        for (int i = 0; i < arrToCount.length; i++) {
+            if (arrToCount[i] == c) {
+                count++;
+            }
+        }
+        return count;
     }
     
     public static boolean isPalindrome(String str) {
         // Your implementation here
         // Note: Ignoring case and non-alphanumeric characters
-        return false;
+        String newStr = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        StringBuilder sb = new StringBuilder(newStr);
+        return newStr.equals(String.valueOf(sb.reverse()));
     }
 }
 
